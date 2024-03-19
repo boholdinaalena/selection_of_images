c00e2e5fd35afad1b159d89e30504afc3bdf38a5
boholdinaalena <nealenachestno@gmail.com>
2024-03-19 16:38:35 +0300

    add reload img

--- src/components/ImageGallary.vue
+++ src/components/ImageGallary.vue
@@ -1,17 +1,18 @@
 <template>
   <VCard class="mx-5 my-4 pa-3">
     <VRow>
-      <VCol v-for="n in 200" :key="n" cols="4" sm="3" md="2" lg="1">
+      <VCol v-for="n in arrKey" :key="n" cols="6" sm="4" md="3" lg="2">
         <VHover
 >
-          <VCard v-if="isVisiable" @clicl="deleteCard()">
+          <VCard class="container" v-if="isVisiable" @click="reloadCard(n)">
             <VImg
               class="gallary-img"
               :src="`https://source.unsplash.com/random/600x600?&${n}&${props.color}`"
-              :lazy-src="`https://source.unsplash.com/random/30x30?&${n}&${props.color}`"
+              :lazy-src="`https://source.unsplash.com/random/600x600?&${n}&${props.color}`"
               aspect-ratio="1"
               cover
             >
+            <img class="reload" src="../assets/img_72363.png" alt="">
               <template v-slot:placeholder>
                 <VRow class="fill-height ma-0" align="center" justify="center">
                   <VProgressCircular
@@ -29,13 +30,42 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref, defineProps,onMounted, reactive } from "vue";
 
 const props = defineProps(["color"]);
 
 const isVisiable = ref(true);
 
-const deleteCard = () => {
-  isVisiable.value = false;
-};
+const gallaryLength = 100;
+let counter = 100
+
+
+const arrKey = reactive(Array.from({length: gallaryLength}, (_, i) =>  i + 1));
+
+const reloadCard = (i) => {
+  const index = arrKey.indexOf(i)
+  arrKey.splice(index, 1, i+counter);
+}
 </script>
+
+<style lang="scss" scoped>
+.reload {
+  width: 100%;
+  padding: 20%;
+  display: block;
+  justify-content: right;
+  background-color: rgba(255, 255, 255, 0.413);
+  visibility: hidden;
+}
+
+.container:hover{
+  .reload {
+  visibility: visible;
+}
+}
+.container::after{
+  .reload {
+  visibility: hidden;
+}
+}
+</style>
